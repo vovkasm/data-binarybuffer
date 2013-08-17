aabc6d92eb242eff97b2cb53a4aa88c49480ad6a
Vladimir Timofeev <vovkasm@gmail.com>
2013-08-18 01:20:52 +0400

    Fix CCFLAGS leak from build environment

--- inc/DistMakeMaker.pm
+++ inc/DistMakeMaker.pm
@@ -1,30 +1,37 @@
 package inc::DistMakeMaker;
 use Moose;
-use Config ();
-use Text::ParseWords 'shellwords';
 
 extends 'Dist::Zilla::Plugin::MakeMaker::Awesome';
 
-override _build_WriteMakefile_args => sub {
+override _build_MakeFile_PL_template => sub {
     my $self = shift;
-    +{
-        %{ super() },
-        $self->_custom_mm_args
-    };
+    my $full_tmpl = super();
+    my $configure_tmpl = $self->_configure_tmpl();
+
+    $full_tmpl =~ s/(^WriteMakefile\(.+?\);\s*$)/$configure_tmpl\n$1/ms;
+    return $full_tmpl;
 };
 
-sub _custom_mm_args {
+sub _configure_tmpl {
+    my $self = shift;
+    my $tmpl = <<'TEMPLATE';
+use Config ();
+use Text::ParseWords 'shellwords';
+
+$WriteMakefileArgs{CONFIGURE} = sub {
+    my %args;
+
     my @CCFLAGS = shellwords($Config::Config{ccflags});
-    my @LDDLFLAGS = shellwords($Config::Config{lddlflags});
-
     push @CCFLAGS, qw/-x c++/;
+    $args{CCFLAGS} = join(' ', @CCFLAGS);
 
-    return (
-        TYPEMAPS => ['perlobject.map'],
-        LIBS     => ['-lstdc++'],
-        CCFLAGS  => join(' ', @CCFLAGS),
-        LDDLFLAGS => join(' ', @LDDLFLAGS),
-    );
+    $args{TYPEMAPS} = ['perlobject.map'];
+    $args{LIBS} = ['-lstdc++'];
+
+    return \%args;
+};
+TEMPLATE
+    return $tmpl;
 }
 
 __PACKAGE__->meta->make_immutable;
